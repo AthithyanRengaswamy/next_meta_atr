4093b3311760c90e24c243b9216a2e962e6c00e0
AthithyanR <athithyan.rengaswamy@kaaylabs.com>
2022-03-25 17:30:10 +0530

    changes

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <Head>
         <title>Athi site</title>
-        <meta property="og:title" content="Athithyan R - main page" />
+        <meta property="og:title" content="Athithyan R - about page" />
         <meta property="og:type" content="video.movie" />
         <meta property="og:url" content="https://github.com/AthithyanRengaswamy" />
         <meta property="og:image" content="https://avatars.githubusercontent.com/u/71121904?s=400&v=4" />
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        index page
+      about page
       </main>
         
     </div>
